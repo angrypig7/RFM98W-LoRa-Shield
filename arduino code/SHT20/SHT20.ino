aaf782111c85dcb0d06412d78222ff2e5ac37b8a
angrypig7 <angrypig777@gmail.com>
2020-01-14 19:16:37 +0900

    tx/rx code

---
+++ arduino code/SHT20/SHT20.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include "DFRobot_SHT20.h"
+
+DFRobot_SHT20    sht20;
+
+void setup(){
+	Serial.begin(115200);
+	Serial.println("SHT20 Example!");
+	sht20.initSHT20(); // Init SHT20 Sensor
+	delay(100);
+	sht20.checkSHT20(); // Check SHT20 Sensor
+}
+
+void loop(){
+	float humd = sht20.readHumidity();
+	float temp = sht20.readTemperature();
+
+	Serial.print("Time:");
+	Serial.print(millis());
+	Serial.print(" Temperature:");
+	Serial.print(temp, 1);
+	Serial.print("C");
+	Serial.print(" Humidity:");
+	Serial.print(humd, 1);
+	Serial.print("%");
+	Serial.println();
+	delay(1000);
+}
